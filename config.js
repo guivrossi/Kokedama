bbd63e15f462bcab362fc61baf4859530ae790c8
guilherme <gui_vrossi@hotmail.com>
2026-05-17 20:57:35 +0200

    adding business phone number

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Atualize com seu número (DDI + DDD + número, sem espaços ou símbolos)
 // Exemplo: 5511999998888 para (11) 99999-8888
 const KOKADAMA_CONFIG = {
-  whatsapp: "5511999999999",
+  whatsapp: "5511992333320",
   instagram: "suacontaaqui",
   brandName: "Kokedama Nice & Lua",
   brandShort: "Nice & Lua",
